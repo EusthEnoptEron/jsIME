d6d59ec6db1bc6ec441bb63f35fa44a045a0adfb
Simon Meer <simon.meer@bluewin.ch>
2013-07-26 18:49:33 +0200

    Clean up server file.

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-var express = require('express');
+var _ = require("underscore"),
+    fs = require("fs"),
+    Edict = require("./lib/edict.js"),
+    express = require('express');
+
 var app = express();
-var fs = require("fs");
-var _ = require("underscore");
-var Edict = require("./lib/edict.js")
 
 // Parse edict
 var dict = JSON.parse(fs.readFileSync(__dirname + "/edict.json"));
 var edict = new Edict(dict);
 var totalMax = 10;
+
+
+// Configure simple server
 app.use(function(req, res, next){
 	res.charset = "utf8";
 	// res.set("Access-Control-Allow-Origin", "*");
  	var text = req.query.text;
+ 	var result = [];
+ 	if(text) {
+	 	result = edict.searchForEntries(text, true);
+	 	result.unshift(text);
 
- 	var result = edict.searchForEntries(text, true);
- 	result.unshift(text);
+	 	// Trim to appropriate length
+	 	if(result.length > totalMax)
+	 		result.splice(totalMax, result.length-totalMax);
 
- 	if(result.length > totalMax)
- 		result.splice(totalMax, result.length-totalMax);
+	 	// Remove duplicates
+	 	result = _.uniq(result);
+ 	}
 
- 	result = _.uniq(result);
  	// Make a JSONP response
 	res.send( req.query.callback + "(" + JSON.stringify(result)  + ")" );
 
 });
 
-
-// app.get('/', function(req, res){
-//   res.send('hello world');
-// });
 
 app.listen(3000);
